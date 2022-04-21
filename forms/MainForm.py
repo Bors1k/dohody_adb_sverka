6598ff0de07b6b0874ae2b88830d1df0e24b5e8d
Bors1k <8916misha@gmail.com>
2022-04-21 11:35:06 +0300

    Выборка файлов

--- forms/MainForm.py
+++ forms/MainForm.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets, QtCore
 from ui_windows.MainWindow import Ui_MainWindow
+import re, os
 
 class MainForm(QtWidgets.QMainWindow):
     def __init__(self):
@@ -7,6 +8,8 @@ class MainForm(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pbutton_choose_files.clicked.connect(self.choose_starter_file)
+        self.files_path = None
+        self.files = None
 
 
     def choose_starter_file(self):
@@ -14,9 +17,46 @@ class MainForm(QtWidgets.QMainWindow):
         self.qfiledlg.setFileMode(QtWidgets.QFileDialog.FileMode.ExistingFile)
         file_path, _ = self.qfiledlg.getOpenFileName(self,filter='Excel (*.xls *.xlsx)')
         splitted = file_path.split("/")
-        len = len(splitted)
-        # file_name = 
-        
-        # if self.qfiledlg.exec() == QtWidgets.QDialog.DialogCode.Accepted:
-        #     path = str(self.qfiledlg.selectedFiles()[0])
-        #     print(path)
+        length = len(splitted)
+        file_name = splitted[length-1]
+        self.files_path = file_path[:-len(file_name)]
+        ubp = file_name[1:6]
+
+        regular = re.compile(r"^C?V?O?"+ubp+r"\.xls.?$")
+
+        self.files = []
+
+        for file_name in os.listdir(self.files_path):
+            if(os.path.isfile(self.files_path + file_name)):
+                if regular.match(file_name) is not None:
+                    self.files.append(file_name)
+
+        if len(self.files) < 3:
+            print("Не хватает файлов")
+        elif len(self.files) > 3:
+            print("В папке присутствует слишком много файлов с данным УБП")
+        else:
+            if self.check_current_files() == True:
+                print("Все файлы получены")
+            else:
+                print("Нет всех нужных файлов")
+                for file in self.files:
+                    print(file)
+
+
+    def check_current_files(self):
+        c_count = 0
+        v_count = 0
+        o_count = 0
+
+        for file in self.files:
+            if file.__contains__("C"):
+                c_count+=1
+            if file.__contains__("V"):
+                v_count+=1
+            if file.__contains__("O"):
+                o_count+=1
+
+        if c_count == 1 and v_count == 1 and o_count == 1:
+            return True
+        return False
